02a9510fac16edef3ec9d9d48b78a7b0c33152db
Juan <jpmbvistro@gmail.com>
2015-05-03 20:00:00 -0600

    Daily Challenge hange

---
+++ katas/kyu8/5-4-2021-3.js
@@ -0,0 +1,8 @@
+/*******************************************************
+=======================Codewars=========================
+Opposites Attract
+https://www.codewars.com/kata/555086d53eac039a2a000083/
+********************************************************/
+function lovefunc(flower1, flower2){
+  return Boolean((flower1 + flower2)%2)
+}
