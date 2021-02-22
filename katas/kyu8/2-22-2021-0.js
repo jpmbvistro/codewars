bb198844f366977f05c1a25c3f29d22864d89d1a
Juan Vistro <>
2021-02-22 18:14:37 -0500

    added some file organization based on kata and kyu as well as even or odd challenge

---
+++ katas/kyu8/2-22-2021-0.js
@@ -0,0 +1,6 @@
+// Even or even_or_odd
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+function even_or_odd(number) {
+  return number%2 ? "Odd" : "Even" //number%2 returns a truthy numbers (not 0) when odd and falsy (0) when even
+}
