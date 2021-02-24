0dc1d4fbd3b73332fbe48d98d2b1391f7c978062
Juan Vistro <jpmbvistro@gmail.com>
2021-02-24 15:11:09 -0500

    daily challenge, summantion of numbers

---
+++ katas/kyu8/2-24-2021-0.js
@@ -0,0 +1,10 @@
+/****
+https://www.codewars.com/kata/55d24f55d7dd296eb9000030/
+sum of numbers
+****/
+
+
+//just used known algorithm rather than brute force summation
+var summation = function (num) {
+  return (num * ( num + 1 ) ) / 2
+}
