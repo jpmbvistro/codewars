c8387f04e448e2c55404a72212a86ef7f97e6df2
Juan Vistro <>
2021-03-07 09:02:56 -0500

    daily code challenge. A good exercise in utilizing character codes to test sequence.

---
+++ katas/kyu6/3-7-2021-0.js
@@ -0,0 +1,14 @@
+/*****************
+Codewars
+https://www.codewars.com/kata/5839edaa6754d6fec10000a2/
+Find the missing Letter
+*******************/
+
+function findMissingLetter(array)
+{
+  for (let i = 0; i < array.length; i++){
+    if(array[i].charCodeAt() !== array[0].charCodeAt()+i){
+      return String.fromCharCode(array[i].charCodeAt()-1)
+    }
+  }
+}
