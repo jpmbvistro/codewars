1dbfe5717b5a1c5d53f2ccbc91917b35ca8a2d02
Juan Vistro <>
2021-03-09 08:39:56 -0500

    Daily challenge for finding first non repeating letter. Made an array mirroring the passed arguement to track passed characters.

---
+++ katas/kyu5/3-9-2021-0.js
@@ -0,0 +1,24 @@
+/*********
+Codewars
+First non-repeating character
+https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/
+
+********/
+function firstNonRepeatingLetter(s) {
+  let booleanLetters = new Array(s.length)
+  booleanLetters.fill(true)
+  for (let i = 0 ; i < s.length ; i++){
+    //checks if the value has already been passed in a previous check, thereby avoiding unnecessary loop
+    if(booleanLetters[i]) {
+      for (let j = i+1 ; j < s.length ; j++) {
+        if( s.charAt(j).toUpperCase() === s.charAt(i).toUpperCase()) {
+          booleanLetters[i] = false
+          booleanLetters[j] = false
+        }
+      }
+      //Since the letters are checked left to right, the first letter that returns true is the first letter without repeats
+      if(booleanLetters[i]){break}
+    }
+  }
+  return s.charAt(booleanLetters.indexOf(true))
+}
