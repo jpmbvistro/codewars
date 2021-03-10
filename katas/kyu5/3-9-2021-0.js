7b80256aad34ac5fc4c663c8165af2ffd3d0f618
Juan Vistro <jpmbvistro>
2021-03-09 19:48:24 -0500

    recommiting with email in config

--- katas/kyu5/3-9-2021-0.js
+++ katas/kyu5/3-9-2021-0.js
@@ -2,7 +2,6 @@
 Codewars
 First non-repeating character
 https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/
-
 ********/
 function firstNonRepeatingLetter(s) {
   let booleanLetters = new Array(s.length)
