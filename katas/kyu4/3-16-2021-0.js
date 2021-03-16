a8e36ffce24c1a8c9cb96d22bff1d8c8555eda28
Juan Vistro <jpmbvistro@gmail.com>
2021-03-16 15:22:07 -0400

    completed daily challenge

--- katas/kyu4/3-16-2021-0.js
+++ katas/kyu4/3-16-2021-0.js
@@ -5,29 +5,28 @@ https://www.codewars.com/kata/58ad317d1541651a740000c5/
  *****In Progress***
 ********************************************************/
 function middlePermutation(s) {
-  //coding and coding..
   let permutations,lengthTemp
   permutations = lengthTemp = s.length
   while (lengthTemp > 1){
     lengthTemp--
     permutations*=lengthTemp
   }
-  let indexMid = Math.floor(permutations/2)
+  let indexMid = Math.floor(permutations/2)-1
 
   s = s.split('')
   let stack = [s[0]]
 
+  //go through each of the letters
   for(let i = 1 ; i < s.length ; i++){
-    let newStack = []
+    let newStack =[]
     //We take each item currently in the stack and insert i at all positions
     stack.forEach((item, index)=>{
-      for( let j = 0 ; j < item.length ; j++){
+      for( let j = 0 ; j <= item.length ; j++){
         newStack.push(item.substring(0,j)+s[i]+item.substring(j))
       }
     })
-
+    stack = newStack
   }
-  console.log(indexMid)
-  console.log(stack)
+  stack.sort()
   return stack[indexMid]
 }
