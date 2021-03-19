50d26e624d808d03c1a498fa6000afea7a855cff
Juan <>
2021-03-19 10:41:54 -0400

    daily challenge

---
+++ katas/kyu5/3-19-2021-0.js
@@ -0,0 +1,24 @@
+/*******************************************************
+=======================Codewars=========================
+Valid Parentheses
+https://www.codewars.com/kata/52774a314c2333f0a7000688
+********************************************************/
+function validParentheses(parens){
+  let stack = []
+  let answer = true
+  let length = parens.length
+  for(let i = 0 ; i < length ;i++){
+    if(parens.charAt(0)==='('){
+      stack.push(parens.charAt(0))
+    } else{
+      if(stack.length>0){
+        stack.pop()
+      } else {
+        answer = false
+        break
+      }
+    }
+    parens = parens.substring(1)
+  }
+  return !stack.length && answer
+}
