d9b4e1fc82a46b645ebf77337a0901a5dfbf2a4e
Juan <jpmbvistro@gmail.com>
2021-03-25 09:06:48 -0400

    Daily Codewars Challenge.

---
+++ katas/kyu6/3-25-2021-0.js
@@ -0,0 +1,12 @@
+/*******************************************************
+=======================Codewars=========================
+Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?
+https://www.codewars.com/kata/58291fea7ff3f640980000f9/
+********************************************************/
+function allContinents(list) {
+  let continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+  list.forEach((item, i)=>{
+    continents = continents.filter(cont=>cont!==item.continent)
+  })
+  return continents.length===0
+}
