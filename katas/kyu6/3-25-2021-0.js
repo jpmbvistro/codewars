1264e7d0660040112f2227fc6f8038c7dce2047d
Juan <jpmbvistro@gmail.com>
2021-03-25 09:08:06 -0400

    Added description

--- katas/kyu6/3-25-2021-0.js
+++ katas/kyu6/3-25-2021-0.js
@@ -2,6 +2,7 @@
 =======================Codewars=========================
 Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?
 https://www.codewars.com/kata/58291fea7ff3f640980000f9/
+Great series to practice HOF
 ********************************************************/
 function allContinents(list) {
   let continents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
