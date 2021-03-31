1a7d3fefaaca7cc422213ee6f0ad35b8423c643f
Juan <jpmbvistro@gmail.com>
2021-03-31 09:11:55 -0400

    Daily Challenge

---
+++ katas/kyu8/3-31-2021-0.js
@@ -0,0 +1,9 @@
+/*******************************************************
+=======================Codewars=========================
+
+Quarter of the year
+https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/
+********************************************************/
+const quarterOf = (month) => {
+  return Math.ceil(month/3)
+}
