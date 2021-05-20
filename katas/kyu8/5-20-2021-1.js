26f6f6b58fa00ebf921cb145975516fd36c16752
Juan <jpmbvistro@gmail.com>
2021-05-20 12:16:48 -0400

    Daily Challenge

---
+++ katas/kyu8/5-20-2021-1.js
@@ -0,0 +1,12 @@
+/*******************************************************
+=======================Codewars=========================
+Pre-FizzBuzz Workout #1
+https://www.codewars.com/kata/56170e844da7c6f647000063
+********************************************************/
+function preFizz(n) {
+  let sol = []
+  for(let i = 1 ; i <=n ; i++){
+    sol.push(i)
+  }
+  return sol
+}
---
+++ katas/kyu8/5-20-2021-2.js
@@ -0,0 +1,8 @@
+/*******************************************************
+=======================Codewars=========================
+Price of Mangoes
+https://www.codewars.com/kata/57a77726bb9944d000000b06
+********************************************************/
+function mango(quantity, price){
+  return Math.floor(quantity/3)*price*2+quantity%3*price
+}
