368936ddb5001399a71ca9a272696cb6e79133fb
JuanV <jpmbvistro@gmail.com>
2021-05-26 08:15:42 -0400

    Daily Challenge

---
+++ katas/kyu8/5-24-2021-0.js
@@ -0,0 +1,8 @@
+/*******************************************************
+=======================Codewars=========================
+Template Strings
+https://www.codewars.com/kata/55a14f75ceda999ced000048
+********************************************************/
+var TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`
+}
