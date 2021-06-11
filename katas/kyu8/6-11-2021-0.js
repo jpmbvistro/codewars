4e58bd971bebb5c4ab3a189425799640ebc22065
JuanV <jpmbvistro@gmail.com>
2021-06-11 11:48:09 -0400

    Daily Challenge

---
+++ katas/kyu8/6-11-2021-0.js
@@ -0,0 +1,8 @@
+/*******************************************************
+=======================Codewars=========================
+Grasshopper - If/else syntax debug
+https://www.codewars.com/kata/57089707fe2d01529f00024a
+********************************************************/
+function checkAlive (health) {
+  return health > 0
+}
