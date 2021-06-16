5300a67a6c6a22acb703c59f25ce7fc2d0d714da
Juan <jpmbvistro@gmail.com>
2021-06-16 10:07:01 -0400

    Daily Challenge

---
+++ katas/kyu8/6-16-2021-0.js
@@ -0,0 +1,8 @@
+/*******************************************************
+=======================Codewars=========================
+Can we divide it?
+https://www.codewars.com/kata/5a2b703dc5e2845c0900005a/train/javascript
+********************************************************/
+function isDivideBy(number, a, b) {
+  return number%a === 0 && number%b === 0
+}
