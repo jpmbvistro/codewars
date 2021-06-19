66ea5bb52d25fef11df54026a63afa11d29ee9c5
Juan <jpmbvistro@gmail.com>
2021-06-19 10:03:50 -0400

    Daily

---
+++ katas/kyu8/6-19-2021-0.js
@@ -0,0 +1,8 @@
+/*******************************************************
+=======================Codewars=========================
+Do I get a bonus?
+https://www.codewars.com/kata/56f6ad906b88de513f000d96/train/javascript
+********************************************************/
+function bonusTime(salary, bonus) {
+  return `£${salary * (bonus ? 10 : 1)}`
+}
